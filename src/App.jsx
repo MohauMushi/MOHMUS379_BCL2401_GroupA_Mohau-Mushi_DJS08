75ebd70d0a4ecd8476d33fd7b694e4b624ba4bb6
Mohau Mushi <david1mushi1@gmail.com>
2024-06-04 11:58:58 +0200

    Created the HostLayout component, and then replace the parent "/host" route's element below with the new HostLayout component you made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetails";
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
 import Dashboard from "./pages/Host/Dashboard";
 import Reviews from "./pages/Host/Reviews";
 import Income from "./pages/Host/Income";
+import HostLayout from "./components/HostLayout";
 
 import "./server";
 
@@ -20,8 +20,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          
-          <Route path="/host" element={<Dashboard />}>
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
