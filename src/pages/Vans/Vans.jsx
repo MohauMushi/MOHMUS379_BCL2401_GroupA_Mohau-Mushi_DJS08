31534d8f8d35dbc0fd824b6b1601d7afe405e637
Mohau Mushi <david1mushi1@gmail.com>
2024-06-04 14:08:15 +0200

    Make the main navbar and host navbar indicate the currently-active route.

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -29,7 +29,9 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options </h1>
-      <div className="">{vanElements}</div>
+      <div className="van-list">
+        {vanElements}
+      </div>
     </div>
   );
 }
