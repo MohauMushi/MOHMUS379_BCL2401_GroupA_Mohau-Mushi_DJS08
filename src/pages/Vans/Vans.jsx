cd61c9e3f41515412d89629c77acfe73ad7daf7c
Mohau Mushi <david1mushi1@gmail.com>
2024-06-05 09:46:24 +0200

    Accessibility Addition(React Router Standalone)

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -64,11 +64,11 @@ export default function Vans() {
   }
 
   if (loading) {
-    return <h1>Loading...</h1>;
+    return <h1 aria-live="polite">Loading...</h1>;
   }
 
   if (error) {
-    return <h1>There was an error: {error.message}</h1>;
+    return <h1 aria-live="assertive">There was an error: {error.message}</h1>;
   }
 
   return (
@@ -77,22 +77,26 @@ export default function Vans() {
       <div className="van-list-filter-buttons">
         <button
           onClick={() => handleFilterChange("type", "simple")}
-          className={`van-type simple ${typeFilter === "simple" ? "selected" : ""}`}
+          className={`van-type simple 
+                        ${typeFilter === "simple" ? "selected" : ""}`}
         >
           Simple
         </button>
         <button
           onClick={() => handleFilterChange("type", "luxury")}
-          className={`van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`}
+          className={`van-type luxury 
+                        ${typeFilter === "luxury" ? "selected" : ""}`}
         >
           Luxury
         </button>
         <button
           onClick={() => handleFilterChange("type", "rugged")}
-          className={`van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`}
+          className={`van-type rugged 
+                        ${typeFilter === "rugged" ? "selected" : ""}`}
         >
           Rugged
         </button>
+
         {typeFilter ? (
           <button
             onClick={() => handleFilterChange("type", null)}
